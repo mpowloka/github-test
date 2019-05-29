07e98194fd504896996594f6c53dd4e7ce9747ed
mpowloka-list <mpowloka@listonic.com>
2019-05-29 16:19:42 +0200

    Tutaj coś napisz

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
+
+        String hello = "Hello world";
+
+        System.out.println(hello);
+
+        System.out.println(hello);
+
     }
 
 }
